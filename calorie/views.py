d7370fc0210fbfa075f0eaf36aabee77cc657c93
ujjawal <ujjawal17032002@gmail.com>
2024-07-31 23:18:49 +0530

    add app template

--- calorie/views.py
+++ calorie/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 
 def homePage(request):
-    return render(request, "base.html")
+    return render(request, "calorie/index.html")
--- caloriefinder/settings.py
+++ caloriefinder/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "calorie.apps.CalorieConfig",
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,10 @@ STATIC_URL = "static/"
 MEDIA_URL = "/images/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images/")
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
